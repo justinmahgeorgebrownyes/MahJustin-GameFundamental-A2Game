5acb60188858b90eb1329d129a12672d0ba47929
justinmahgeorgebrownyes <justin.mah2@georgebrown.ca>
2022-04-12 19:55:51 -0400

    Add project files.

---
+++ GAME1017_EmptyEngine/PlatformPlayer.cpp
@@ -0,0 +1,237 @@
+#include "PlatformPlayer.h"
+#include "EventManager.h"
+#include "TextureManager.h"
+#include <cmath>
+#include "States.h"
+
+PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d, const int w, const int h) : AnimatedSpriteObject(s, d),
+
+m_state(STATE_JUMPING), m_isGrounded(false), m_isFacingLeft(false),
+m_maxVelX(9.0), m_maxVelY(JUMPFORCE), m_grav(GRAVITY), m_drag(0.85)
+
+
+{
+	m_accelX = m_accelY = m_velX = m_velY = 0.0;
+
+	char key;
+	m_playerkey = 'a';
+	for (int y = 0; y < 2; y++)
+	{
+		for (int x = 0; x < 9; x++) {
+
+			key = (char)'a' + x + (9 * y);
+
+
+			m_tiles.emplace(key, new Tile({ x * w, y * h, w, h },
+				{ 0.0f, 0.0f, (float)64, (float)64 }, 0, 0));
+		}
+	}
+
+
+
+
+
+
+}
+
+
+void PlatformPlayer::Update()
+{
+	switch (m_state) { //inside each case is the behaving in and transiton from that state
+	case STATE_IDLING:
+		//transitoin to run
+
+		m_playerkey = (char)'a' + 9 + (0 % 4);
+
+
+
+		if (EVMA::KeyPressed(SDL_SCANCODE_A) || EVMA::KeyPressed(SDL_SCANCODE_D)) {
+
+
+
+			m_state = STATE_RUNNING;
+			//SetAnimation(?,?,?,?);
+			SetAnimation(288, 480, 64, 64);
+		}
+		//transition to jump
+		else if (EVMA::KeyPressed(SDL_SCANCODE_SPACE) && m_isGrounded) {
+
+			m_accelY = -JUMPFORCE;
+			m_isGrounded = false;
+			m_state = STATE_JUMPING;
+			//set animation (?,?,?,?);
+			SetAnimation(288, 480, 64, 64);
+		}
+
+
+		break;
+	case STATE_RUNNING:
+
+		m_playerkey = (char)'a' + 0 + (m_frame % 8);
+
+
+		//move on ground
+		if (EVMA::KeyHeld(SDL_SCANCODE_A)) {
+			m_accelX = -1.5;
+			if (!m_isFacingLeft)
+				m_isFacingLeft = true;
+		}
+		else if (EVMA::KeyHeld(SDL_SCANCODE_D)) {
+			m_accelX = 1.5;
+			if (m_isFacingLeft)
+				m_isFacingLeft = false;
+		}
+
+
+
+		//transition to idle
+		if (!EVMA::KeyHeld(SDL_SCANCODE_A) && !EVMA::KeyHeld(SDL_SCANCODE_D)) {
+
+			m_state = STATE_IDLING;
+
+		}
+
+		// 
+		// 
+		//transitiont to jump
+		else if (EVMA::KeyPressed(SDL_SCANCODE_SPACE) && m_isGrounded) {
+
+			m_accelY = -JUMPFORCE;
+			m_isGrounded = false;
+			m_state = STATE_JUMPING;
+			//set animation (?,?,?,?);
+			SetAnimation(288, 480, 64, 64);
+		}
+
+
+		break;
+	case STATE_JUMPING:
+
+
+		m_playerkey = (char)'a' + 17;
+
+		if (EVMA::KeyHeld(SDL_SCANCODE_A)) {
+
+			m_accelX = -1.5;
+			if (!m_isFacingLeft)
+				m_isFacingLeft = true;
+
+		}
+		else if (EVMA::KeyHeld(SDL_SCANCODE_D)) {
+
+			m_accelX = 1.5;
+			if (m_isFacingLeft)
+				m_isFacingLeft = false;
+		}
+
+		if (m_isGrounded) {
+			m_state = STATE_RUNNING;
+
+
+			//setanimation(?,?,?,?);
+			SetAnimation(288, 480, 64, 64);
+
+
+
+		}
+
+		break;
+	}
+
+
+	m_velX += m_accelX;
+	m_velX *= (m_isGrounded ? m_drag : 1.0);
+	m_velX = std::min(std::max(m_velX, -m_maxVelX), m_maxVelX);
+	m_dst.x += (float)m_velX;
+
+	//yaxis
+	m_velY += m_accelY + m_grav;
+	m_velY = std::min(std::max(m_velY, -m_maxVelY), m_maxVelY);
+	m_dst.y += (float)m_velY;
+
+	m_accelX = m_accelY = 0.0;
+	Animate();
+}
+
+void PlatformPlayer::Render()
+{
+	//to animate the sprite, use sdl_rendercopyexf() and you wll ahve to access the texture manager and pass in a key such as "player".
+
+//	SDL_RenderCopyExF(Engine::Instance().GetRenderer(), TEMA::GetTexture("player"), &m_src, &m_dst, m_sprite, nullptr, SDL_FLIP_NONE);
+
+
+
+
+	// 
+	// 
+	// 
+	//but for this part, w'erd going to use a colored square
+
+
+	SDL_SetRenderDrawColor(Engine::Instance().GetRenderer(), 255, 0, 0, 255);
+	//	SDL_RenderFillRectF(Engine::Instance().GetRenderer(), &m_dst);
+
+	Tile* temp = m_tiles[m_playerkey]->Clone();
+
+	temp->SetXY(m_dst.x, m_dst.y);
+
+	SDL_RenderCopyExF(Engine::Instance().GetRenderer(), TEMA::GetTexture("player"), temp->GetSrc(), temp->GetDst(), 0, nullptr, SDL_FLIP_NONE);
+
+}
+
+void PlatformPlayer::Stop()
+{
+	StopX();
+	StopY();
+}
+
+void PlatformPlayer::StopX()
+{
+	m_velX = 0.0;
+}
+
+void PlatformPlayer::StopY()
+{
+	m_velY = 0.0;
+}
+
+void PlatformPlayer::SetAccelX(double a)
+{
+	m_accelX = a;
+}
+
+void PlatformPlayer::SetAccelY(double a)
+{
+	m_accelY = a;
+}
+
+void PlatformPlayer::SetX(float x)
+{
+	m_dst.x = x;
+}
+
+void PlatformPlayer::SetY(float y)
+{
+	m_dst.y = y;
+}
+
+void PlatformPlayer::SetGrounded(bool g)
+{
+	m_isGrounded = g;
+
+}
+
+bool PlatformPlayer::IsGrounded()
+{
+	return m_isGrounded;
+}
+
+double PlatformPlayer::GetVelX()
+{
+	return m_velX;
+}
+
+double PlatformPlayer::GetVelY()
+{
+	return m_velY;
+}
---
+++ GAME1017_EmptyEngine/States.cpp
@@ -0,0 +1,179 @@
+#include "States.h"
+#include "StateManager.h"
+#include "TextureManager.h"
+#include "Engine.h"
+#include "EventManager.h"
+#include "CollisionManager.h"
+#include "SoundManager.h"
+#include "Primitives.h"
+#include "Button3.h"
+#include "PlatformPlayer.h"
+#include "GameObject.h"
+#include <iostream>
+using namespace std;
+
+void State::Render()
+{
+	SDL_RenderPresent(Engine::Instance().GetRenderer());
+}
+
+void State::Resume() {}
+
+GameObject* State::GetGo(const std::string& s)
+{ // Using 'std' just to show origin.
+	auto it = std::find_if(m_objects.begin(), m_objects.end(),
+		// Lambda expression/function. An in-function function.
+		[&](const std::pair<std::string, GameObject*>& element)
+		{
+			return element.first == s;
+		}
+	); // End of find_if.
+	if (it != m_objects.end())
+		return it->second;
+	else return nullptr;
+}
+
+auto State::GetIt(const std::string& s)
+{
+	auto it = std::find_if(m_objects.begin(), m_objects.end(),
+		[&](const std::pair<std::string, GameObject*>& element)
+		{
+			return element.first == s;
+		});
+	return it;
+}
+
+// Begin TitleState
+TitleState::TitleState() {}
+
+void TitleState::Enter()
+{
+	TEMA::Load("Img/button.png", "play");
+	m_objects.push_back(pair<string, GameObject*>("play",
+		new PlayButton({ 0, 0, 400, 100 }, { 412, 350, 200, 50 }, "play")));
+}
+
+void TitleState::Update()
+{
+	for (auto const& i : m_objects)
+	{
+		i.second->Update();
+		if (STMA::StateChanging()) return;
+	}
+}
+
+void TitleState::Render()
+{
+	SDL_SetRenderDrawColor(Engine::Instance().GetRenderer(), 64, 0, 16, 255);
+	SDL_RenderClear(Engine::Instance().GetRenderer());
+	for (auto const& i : m_objects)
+		i.second->Render();
+	State::Render();
+}
+
+void TitleState::Exit()
+{
+	TEMA::Unload("play");
+	for (auto& i : m_objects)
+	{
+		delete i.second;
+		i.second = nullptr;
+	}
+	m_objects.clear();
+	m_objects.shrink_to_fit();
+}
+// End TitleState
+
+// Begin GameState
+GameState::GameState() {}
+
+void GameState::Enter() // Used for initialization.
+{
+
+	TEMA::Load("Img/Tiles.png", "tiles");
+
+	//loda player image and give it a string key of "player"..
+
+	TEMA::Load("Img/Player.png", "player");
+
+
+	m_objects.push_back(pair<string, GameObject*>("level", new TiledLevel(
+		24, 32, 32, 32, "Dat/Tiledata.txt", "Dat/Level1.txt", "tiles")));
+	m_objects.push_back(pair<string, GameObject*>("player", new PlatformPlayer({ 0,0,0,0 }, { 288, 480, 64, 64 }, 128, 128)));
+
+
+}
+
+void GameState::Update()
+{
+	if (EVMA::KeyPressed(SDL_SCANCODE_X))
+	{
+		STMA::ChangeState(new TitleState());
+		return;
+	}
+	for (auto const& i : m_objects)
+	{
+		i.second->Update();
+		if (STMA::StateChanging()) return;
+	}
+	//check collision
+	PlatformPlayer* pObj = static_cast<PlatformPlayer*>(GetGo("player"));
+	SDL_FRect* pBound = pObj->GetDst();
+	TiledLevel* pLevel = static_cast<TiledLevel*>(GetGo("level"));
+
+	for (unsigned int i = 0; i < pLevel->GetObstacles().size(); i++) {
+		SDL_FRect* pTile = pLevel->GetObstacles()[i]->GetDst();
+		if (COMA::AABBCheck(*pBound, *pTile)) {
+
+			bool colTop = false;
+
+			//top side
+			if ((pBound->y + pBound->h) - (float)pObj->GetVelY() <= pTile->y) {
+				colTop = true;
+				pObj->StopY();
+				pObj->SetY(pTile->y - pBound->h);
+				pObj->SetGrounded(true);
+			}
+			//	if colliding with bottom side of tile
+			else if (pBound->y - (float)pObj->GetVelY() >= (pTile->y + pTile->h)) {
+				pObj->StopY();
+				pObj->SetY(pTile->y + pTile->h);
+			}
+			//left side
+			if ((pBound->x + pBound->w) - (float)pObj->GetVelX() <= pTile->x) {
+				pObj->StopX();
+				pObj->SetX(pTile->x - pTile->w);
+			}
+			//right side
+			else if (pBound->x - (float)pObj->GetVelX() >= (pTile->x + pTile->w)) {
+				pObj->StopX();
+				pObj->SetX(pTile->x + pTile->w);
+			}
+		}
+	}
+
+}
+
+void GameState::Render()
+{
+	SDL_SetRenderDrawColor(Engine::Instance().GetRenderer(), 0, 0, 0, 255);
+	SDL_RenderClear(Engine::Instance().GetRenderer());
+	for (auto const& i : m_objects)
+		i.second->Render();
+	if (dynamic_cast<GameState*>(STMA::GetStates().back()))
+		State::Render();
+}
+
+void GameState::Exit()
+{
+	for (auto& i : m_objects)
+	{
+		delete i.second;
+		i.second = nullptr;
+	}
+	m_objects.clear();
+	m_objects.shrink_to_fit();
+}
+
+void GameState::Resume() {}
+// End GameState
